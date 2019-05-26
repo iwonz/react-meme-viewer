fc4e419d24f63836f9955f5f3b4543c164c532d6
ivan.zimin <ivan.zimin@billing.ru>
2019-05-26 21:27:26 +0300

    Updates

---
+++ precache-manifest.0d0f6f5513cd0e46eaf2e79665e70318.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e3985a216df76241b366c9d9462c6e1",
+    "url": "/react-meme-viewer/index.html"
+  },
+  {
+    "revision": "539cb71d39c5cb588704",
+    "url": "/react-meme-viewer/static/css/main.bf079c78.chunk.css"
+  },
+  {
+    "revision": "2542c85a5b593ab7953e",
+    "url": "/react-meme-viewer/static/js/2.c270be6f.chunk.js"
+  },
+  {
+    "revision": "539cb71d39c5cb588704",
+    "url": "/react-meme-viewer/static/js/main.196fd9fe.chunk.js"
+  },
+  {
+    "revision": "4c36e295ba77bfc31059",
+    "url": "/react-meme-viewer/static/js/runtime~main.808929a1.js"
+  }
+]);
